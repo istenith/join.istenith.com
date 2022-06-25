dd407c0184382248dffd7fdb2df3cd0faa1706a3
lucifer1708 <20bce091@nith.ac.in>
2022-06-25 09:19:55 +0530

    file size limit increased from 2mb to 3mb

--- forms/models.py
+++ forms/models.py
@@ -6,9 +6,9 @@ from django.core.exceptions import ValidationError
 
 
 def file_size(value):  # add this to some file where you can import it from
-    limit = 2 * 1024 * 1024
+    limit = 3 * 1024 * 1024
     if value.size > limit:
-        raise ValidationError('File too large. Size should not exceed 2 MiB.')
+        raise ValidationError('File too large. Size should not exceed 3 MiB.')
 
 
 class Registeration(models.Model):
