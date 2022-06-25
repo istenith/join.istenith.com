3f5a3e8e267a0486ec35b76b2bf4913cccd7c330
abhaysood1793 <abhayasood99@gmail.com>
2022-06-26 01:16:54 +0530

    latest

--- forms/views.py
+++ forms/views.py
@@ -1,7 +1,53 @@
+from http.client import HTTPResponse
+from urllib.request import Request
 from django.shortcuts import render
-
+from .forms import RegistrationForm
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'form.html')
+    # form = RegistrationForm(request.POST,request.FILES)
+    # # if form.is_valid():
+    # print(form)
+    # registeration = form.save()
+    # registeration.save()
+    #     # return HTTPResponse("form saved")
+    # # else:
+    # context = {'form':form}
+    # print(" index views")
+
+    if request.method=="POST":
+        form = RegistrationForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form = form.save()
+            return HTTPResponse("form ")
+        else:
+            print("not valid")
+    else:
+        form = RegistrationForm()
+        print("not post")
+    return render(request, 'form.html', {'form':form})
+
+
+# def submit(request):
+#     # form = RegistrationForm(request.POST,request.FILES)
+#     # # if form.is_valid():
+#     # print(form)
+#     # registeration = form.save()
+#     # registeration.save()
+#     #     # return HTTPResponse("form saved")
+#     # # else:
+#     # context = {'form':form}
+#     # print(" index views")
+
+#     if request.method=="POST":
+
+#         form = RegistrationForm(request.POST)
+#         if form.is_valid():
+#             form = form.save()
+#             return HTTPResponse("form ")
+#         else:
+#             print("form not valid")
+    
+#     return render(request, 'form.html', {'form':form})
