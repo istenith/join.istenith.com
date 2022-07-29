e6b03415b8fe27760a1ccf0bf797fc475d375ed5
Swastik <swastkk@gmail.com>
2022-07-29 22:37:12 +0530

    minor-working-errors-resolved

--- static/js/index.js
+++ static/js/index.js
@@ -1,31 +1,47 @@
 var $input;
 
+// function validate_email() {
+//   var userEmail = document.getElementById("id_email").value;
+//   var emailStart = userEmail.substring(0, 2);
+//   var regx = /21/;
+//   if (regx.test(emailStart)) {
+//   } else {
+//   }
+
 function onInputFocus(event) {
   var $target = $(event.target);
   var $parent = $target.parent();
-  $parent.addClass('input--filled');
-};
+  $parent.addClass("input--filled");
+}
 
 function onInputBlur(event) {
   var $target = $(event.target);
   var $parent = $target.parent();
 
-  if (event.target.value.trim() === '') {
-    $parent.removeClass('input--filled');
+  if (event.target.value.trim() === "") {
+    $parent.removeClass("input--filled");
   }
-};
+}
 
-$(document).ready(function() {
-  $input = $('.input__field');
-  
+$(document).ready(function () {
+  $input = $(".input__field");
+
   // in case there is any value already
-  $input.each(function(){
-    if ($input.val().trim() !== '') {
+  $input.each(function () {
+    if ($input.val().trim() !== "") {
       var $parent = $input.parent();
-      $parent.addClass('input--filled');
+      $parent.addClass("input--filled");
     }
   });
-  
-  $input.on('focus', onInputFocus);
-  $input.on('blur', onInputBlur);
+
+  $input.on("focus", onInputFocus);
+  $input.on("blur", onInputBlur);
 });
+
+// Declared the regx variable
+//  declared a variable in js having the starting 2 characters of the email via getelement method
+//  now if statment used and if(regX== true), then run the success page ki submit hogya successfully.
+//  if the statemnet is false then throw the same type error that we get in the Django emailValidator
+//  return the same home page to refill the form easy af
+// aidjafha
+// disojfioa
