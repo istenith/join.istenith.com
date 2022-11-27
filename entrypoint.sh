fafa63fe317272627a28cf8d70e60008d6e85a42
lucifer1708 <20bce091@nith.ac.in>
2022-11-27 09:11:44 +0530

    fixed entrypoint shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn cryptic.wsgi:application --bind 0.0.0.0:8000
+gunicorn main.wsgi:application --bind 0.0.0.0:8000
