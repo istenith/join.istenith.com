7fbe2367ed8d7060b3a57db99e991b9576cc62b7
lucifer1708 <20bce091@nith.ac.in>
2022-12-11 04:04:39 +0530

    csrf trusted origins

--- main/settings.py
+++ main/settings.py
@@ -19,8 +19,8 @@ CSRF_TRUSTED_ORIGINS = [
     'https://join.istenith.com',
     'http://join.istenith.com',
     'http://127.0.0.1:8000/',
-    'https://interview-iste.azurewebsites.net/',
-    'http://interview-iste.azurewebsites.net/'
+    'https://interview-iste.azurewebsites.net',
+    'http://interview-iste.azurewebsites.net'
 ]
 
 INSTALLED_APPS = [
