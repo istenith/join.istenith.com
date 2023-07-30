e1c680ddfc0581827b5b14c70da84d6b7fcbeacd
lucifer1708 <20bce091@nith.ac.in>
2023-07-30 17:36:55 +0530

    made some changes in workfow

--- main/settings.py
+++ main/settings.py
@@ -1,25 +1,24 @@
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://join.istenith.com",
     "http://join.istenith.com",
-    "http://0.0.0.0:8081"
+    "http://0.0.0.0:8081",
 ]
 
 INSTALLED_APPS = [
