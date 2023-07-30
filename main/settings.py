b91fa13157b0938dac875346661c635cc6d21a00
sumitdhiman <hello@sumitdhiman.in>
2023-07-30 10:26:21 +0000

    .env created and settings updated
    Signed-off-by: sumitdhiman <hello@sumitdhiman.in>

--- main/settings.py
+++ main/settings.py
@@ -8,10 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a)*#0p#x9s4_g!p5mrh*4bars7z^ub(^8t@e7mn9cao24r(e#+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ['*']
 
