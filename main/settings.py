1b21c293db4070784d3e24c2df5d47fe8ac3eff9
Swastik Sharma <swastkk@gmail.com>
2023-07-30 16:21:29 +0530

    Settings updated and error resolved
    Signed-off-by: Swastik Sharma <swastkk@gmail.com>

--- main/settings.py
+++ main/settings.py
@@ -1,8 +1,10 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
