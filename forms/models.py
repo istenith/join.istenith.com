aeb3512a77fb0ebe2965cc288df191c46534d640
Swastik Sharma <81990329+swastkk@users.noreply.github.com>
2023-08-31 12:39:30 +0530

    Update models.py for 23 batch rollno.
    Changed from GitHub itself.

--- forms/models.py
+++ forms/models.py
@@ -37,7 +37,7 @@ class Registeration(models.Model):
         unique=True,
         validators=[
             RegexValidator(
-                regex=r'^[2][2][a-zA-Z]{3}\d{3}@nith[.]ac[.]in$',
+                regex=r'^[2][3][a-zA-Z]{3}\d{3}@nith[.]ac[.]in$',
                 message=
                 "Only freshers with correct college email addresses are authorised."
             )
