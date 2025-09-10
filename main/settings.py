7d1cf94c629d238f9e1eeda2234e7c95fcdc069c
mehulambastha <mehul.213amb@gmail.com>
2025-09-10 14:54:35 +0530

    [10/09/2025] Change static serve url

--- main/settings.py
+++ main/settings.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
@@ -114,7 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_URL = "/static/"
+STATIC_URL = "/join-static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
